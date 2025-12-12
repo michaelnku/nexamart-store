d178f93c5ececd1b8fbd672fb04307df7c77ad94
michael <nkumichael1@gmail.com>
2025-12-12 17:54:05 +0100

    initial commit

--- src/app/(dashboard)/market-place/dashboard/seller/products/new/page.tsx
+++ src/app/(dashboard)/market-place/dashboard/seller/products/new/page.tsx
@@ -1,10 +1,13 @@
-import React from "react";
-import ProductForm from "../../../_components/ProductForm";
+import ProductForm from "@/app/(dashboard)/market-place/_components/ProductForm";
+import { prisma } from "@/lib/prisma";
 
-const page = () => {
+const page = async () => {
+  const categories = await prisma.category.findMany({
+    orderBy: { name: "asc" },
+  });
   return (
     <div className="w-full min-h-screen bg-zinc-50 dark:bg-zinc-900">
-      <ProductForm />
+      <ProductForm categories={categories} />
     </div>
   );
 };
