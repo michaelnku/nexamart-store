3a36b0e0540f406ced9771d411f8d543c5ac397b
michael <nkumichael1@gmail.com>
2026-02-17 19:35:39 +0100

    updated reviews logic

--- src/app/marketplace/dashboard/seller/products/[productId]/update/page.tsx
+++ src/app/marketplace/dashboard/seller/products/[productId]/update/page.tsx
@@ -43,6 +43,7 @@ export default async function Page({
       reviews: {
         select: {
           id: true,
+          orderId: true,
           rating: true,
           comment: true,
           createdAt: true,
