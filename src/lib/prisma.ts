d0679b7fb8b4f9b3d3f8dce45894aeb929169d4c
michael <nkumichael1@gmail.com>
2026-02-23 20:51:30 +0100

    adding adapter

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,33 @@
-import { PrismaClient } from "@/generated/prisma";
-import { withAccelerate } from "@prisma/extension-accelerate";
+// import { PrismaClient } from "@/generated/prisma";
+// import { withAccelerate } from "@prisma/extension-accelerate";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+// const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma =
+// export const prisma =
+//   globalForPrisma.prisma ||
+//   new PrismaClient({
+//     accelerateUrl: process.env.ACCELERATE_URL,
+//   }).$extends(withAccelerate());
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+
+const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    accelerateUrl: process.env.ACCELERATE_URL,
-  }).$extends(withAccelerate());
+    adapter,
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
