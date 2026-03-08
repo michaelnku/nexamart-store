483c66d0a2ab5b217f04b3d1f7b0ef7da1135fc1
michael <nkumichael1@gmail.com>
2026-03-08 05:52:52 +0100

    refining store ui

--- src/app/(site)/store/[slug]/page.tsx
+++ src/app/(site)/store/[slug]/page.tsx
@@ -161,14 +161,14 @@ const page = async ({ params }: StoreFrontProps) => {
 
         <section className="flex flex-col items-center gap-4 text-center">
           {/* Logo */}
-          <div className="w-32 h-32 rounded-full overflow-hidden border shadow bg-gray-50 -mt-20">
+          <div className="w-32 h-32 rounded-full overflow-hidden border rounded-full border-[#125c99] shadow bg-gray-50 z-50 -mt-20">
             {store.logo ? (
               <Image
                 src={store.logo}
                 alt={store.name}
                 width={128}
                 height={128}
-                className="object-cover w-full h-full"
+                className="object-cover w-full h-full "
               />
             ) : (
               <div className="w-full h-full flex items-center justify-center text-gray-400">
