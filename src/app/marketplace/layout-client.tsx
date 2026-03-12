d9e7ca89fa2aacf4f2c6e47c60d424d4bf4ac75c
michael <nkumichael1@gmail.com>
2026-03-12 13:24:16 +0100

    tightened release job

--- src/app/marketplace/layout-client.tsx
+++ src/app/marketplace/layout-client.tsx
@@ -12,13 +12,13 @@ export default function MarketplaceLayoutClient({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-dvh flex-col overflow-x-clip">
+    <div className="flex h-dvh min-h-0 flex-col overflow-hidden">
       <MarketPlaceNavbar initialUser={user} />
 
-      <div className="mx-auto flex min-h-0 w-full flex-1 items-stretch px-0 sm:px-3 lg:px-5 xl:px-8 2xl:max-w-[1920px]">
+      <div className="mx-auto flex min-h-0 w-full flex-1 items-stretch overflow-hidden px-0 sm:px-3 lg:px-5 xl:px-8 2xl:max-w-[1920px]">
         <DashboardSidebar initialUser={user} />
 
-        <main className="min-w-0 flex-1 overflow-y-auto overscroll-contain px-3 py-3 sm:px-4 sm:py-4 md:px-5 md:py-5 lg:px-6 lg:py-6 xl:px-8">
+        <main className="min-h-0 min-w-0 flex-1 overflow-y-auto overscroll-contain px-3 py-3 sm:px-4 sm:py-4 md:px-5 md:py-5 lg:px-6 lg:py-6 xl:px-8">
           {children}
         </main>
       </div>
